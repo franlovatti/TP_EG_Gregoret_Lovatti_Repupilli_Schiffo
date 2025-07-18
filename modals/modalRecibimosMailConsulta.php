2dff6896ca41d358a34f233ab0782f9e5e4e3197
IriDev <irirepupilli123@gmail.com>
2025-07-18 11:54:23 -0300

    modal consulta exitosa

---
+++ modals/modalRecibimosMailConsulta.php
@@ -0,0 +1,16 @@
+<div class="modal fade" id="modalRecibimosMailConsulta" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Recibimos su consulta</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Gracias por contactarse con nosotros, pronto una persona del staff le enviará una respuesta.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
