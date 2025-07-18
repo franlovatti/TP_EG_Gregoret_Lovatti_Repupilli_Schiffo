c27594cac4ca6c5f7530db62883bb3e477816d90
IriDev <irirepupilli123@gmail.com>
2025-07-18 12:14:03 -0300

    agregue modal terminosYcondiciones

---
+++ modals/modalTerminosCondiciones.php
@@ -0,0 +1,56 @@
+<div class="modal fade" id="modalTerminosCondiciones" tabindex="-1">
+  <div class="modal-dialog modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Términos y condiciones</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p><b>1. ACEPTACIÓN DE LOS TÉRMINOS</b></h4>
+        <p>El acceso y uso del sitio web del Shopping [Nombre del Shopping] (en adelante, "el Shopping") 
+          implica la aceptación plena y sin reservas de todos y cada uno de los presentes Términos y Condiciones por parte del usuario. 
+          Si no está de acuerdo con alguno de estos términos, le recomendamos no utilizar este sitio.
+        </p>
+
+        <p><b>2. USO DEL SITIO</b></p>
+        <p>El usuario se compromete a utilizar el sitio web y los servicios disponibles conforme a la legislación vigente, la moral, 
+          el orden público y estos Términos y Condiciones. 
+          Queda expresamente prohibido el uso del sitio con fines ilícitos o que puedan causar daños a terceros o al funcionamiento del sitio.
+        </p>
+        <p><b>3. MODIFICACIONES</b></p>
+        <p>El Shopping se reserva el derecho de modificar en cualquier momento estos Términos y Condiciones, 
+          así como la estructura, diseño, funcionalidades o contenidos del sitio web. Las modificaciones tendrán efecto a partir de su publicación.
+        </p>
+        <p><b>4. REGISTRO Y DATOS PERSONALES</b></p>
+        <p>En caso de que el sitio requiera registro para acceder a determinados servicios (como recibir promociones, participar de sorteos o acumular puntos), 
+          el usuario se compromete a proporcionar información veraz y actualizada. 
+          Los datos personales serán tratados conforme a la Política de Privacidad disponible en este sitio.
+        </p>
+        <p><b>5. PROPIEDAD INTELECTUAL</b></p>
+        <p>Todos los contenidos del sitio (textos, imágenes, logos, marcas, software, etc.) son propiedad del Shopping o de terceros que han autorizado su uso. 
+          Queda prohibida su reproducción, distribución o modificación sin autorización previa y por escrito.
+        </p>
+        <p><b>6. ENLACES A SITIOS DE TERCEROS</b></p>
+        <p>Este sitio puede contener enlaces a sitios web de terceros. 
+          El Shopping no se hace responsable por el contenido, políticas o prácticas de dichos sitios. 
+          La inclusión de estos enlaces no implica aprobación o respaldo por parte del Shopping.
+        </p>
+        <p><b>7. LIMITACIÓN DE RESPONSABILIDAD</b></p>
+        <p>El Shopping no garantiza la disponibilidad permanente del sitio, ni se hace responsable por interrupciones, errores, virus u otros elementos que puedan afectar el funcionamiento del sitio o los dispositivos del usuario. 
+          El uso del sitio es bajo exclusiva responsabilidad del usuario.
+        </p>
+        <p><b>8. PROMOCIONES Y DESCUENTOS</b></p>
+        <p>Las promociones, descuentos y sorteos publicados en este sitio están sujetos a condiciones específicas, vigencias y disponibilidad. 
+          El Shopping se reserva el derecho de modificarlos o cancelarlos sin previo aviso.
+        </p>
+        <p><b>9. JURISDICCIÓN Y LEY APLICABLE</b></p>
+        <p>Estos Términos y Condiciones se rigen por las leyes de la República Argentina. 
+          Cualquier controversia será sometida a la jurisdicción de los tribunales ordinarios de la Ciudad.
+        </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
