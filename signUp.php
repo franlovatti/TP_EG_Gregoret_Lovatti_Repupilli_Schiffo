9b1aca2ffc80b1e3c2aa3d312f6ce2d3750d8262
franlovatti <franciscolovatti08@gmail.com>
2026-03-25 20:45:21 -0300

    fix: signUp correccion de la correccion

--- signUp.php
+++ signUp.php
@@ -29,7 +29,6 @@ function signUp() {
       $query = "INSERT INTO usuario (mail_usuario, clave_usuario, tipo_usuario, estado, categoria, selector_verificado, token_verificado, verificado) VALUES ('$email', '$clave', '$tipo', '$estado', 'inicial', '$selector', '$token_hash', '0')";
       $resultados = mysqli_query($conexion, $query) or die("Hubo un error con la transacción:".mysqli_error($conexion));
       if ($resultados) {
-        //NO ANDA LA VERIFICACION POR MAIL PORQUE NO TENEMOS LA PAGINA SUBIDA, PERO DEBERIA ENVIAR EL CORREO CON EL LINK DE VERIFICACION
         $asunto = "Verificacion de cuenta";
         $mensaje = "<p>Hola, te enviamos este correo para verificar tu cuenta. Por favor, haz clic en el siguiente enlace para verificarla:</p>
         <p><a href='http://localhost/archivosXampp/TP_EG_Gregoret_Lovatti_Repupilli_Schiffo/front/home.php?selector_verificado=".$selector."&token_verificado=".$token."'>Verificar cuenta</a></p>
@@ -44,55 +43,6 @@ function signUp() {
         }
       } else {
         $signUp_error = "Error al registrar el usuario.";
-      }
-    }
-  }
-  mysqli_close($conexion);
-}
-
-function signUpD() {
-  require_once '../conexion.php';
-  error_reporting(E_ERROR | E_PARSE); // Muestra solo errores fatales y errores de análisis
-  ini_set('display_errors', 0);       // No mostrar errores al usuario
-  global $signUp_error;
-  $email = trim($_POST['correo']);
-  $clave = $_POST['clave'];
-  $clave2 = $_POST['clave2'];
-  if ($clave !== $clave2) {
-    $signUp_error = "Las contraseñas no coinciden.";
-  }else {
-    $clave = password_hash($clave, PASSWORD_DEFAULT);
-    $mantenerSesionIniciada = $_POST['mantenerSesionIniciada'];
-    $query = "SELECT * FROM usuario WHERE mail_usuario='$email' ";
-    $resultados = mysqli_query($conexion,$query) or die("Hubo un error con la transacción:".mysqli_error($conexion));
-    if(mysqli_num_rows($resultados) > 0){
-      $signUp_error = "El usuario ya existe.";
-    } else {
-
-      //TUVE QUE AGREGAR EL CAMPO DE VERIFICADO EN 0, DESPUES CUANDO EL ADMIN APRUEBA SE SETEA EN 1
-      //NO PODEMOS VERIFICAR CON MAIL PQ TODAVIA NO TENEMOS LA PAGINA SUBIDA
-      $query = "INSERT INTO usuario (mail_usuario, clave_usuario, tipo_usuario, estado, categoria, selector_verificado, token_verificado, verificado) VALUES ('$email', '$clave', 'dueño', 'pendiente', 'inicial', '', '', '0')";
-      $resultados = mysqli_query($conexion, $query) or die("Hubo un error con la transacción:".mysqli_error($conexion));
-      if ($resultados) {
-        $query = "SELECT * FROM usuario WHERE mail_usuario='$email' ";
-        $resultados = mysqli_query($conexion, $query) or die("Hubo un error con la transacción:".mysqli_error($conexion));
-        $fila = mysqli_fetch_array($resultados);
-        $_SESSION['usuario'] = $fila['mail_usuario'];
-        $_SESSION['tipoUsuario'] = $fila['tipo_usuario'];
-        $_SESSION['idUsuario'] = $fila['id_usuario'];
-        $_SESSION['estado'] = $fila['estado'];
-        if ($mantenerSesionIniciada == 'si') {
-          setcookie('mantenerSesionIniciada', 'si', time() + (60 * 60 * 24 * 365));
-          setcookie('usuario', $fila['mail_usuario'], time() + (60 * 60 * 24 * 365));
-          setcookie('tipoUsuario', $fila['tipo_usuario'], time() + (60 * 60 * 24 * 365));
-          setcookie('idUsuario', $fila['id_usuario'], time() + (60 * 60 * 24 * 365));
-          setcookie('estado', $fila['estado'], time() + (60 * 60 * 24 * 365));
-        }
-        $signUp_error = '';
-        echo '<meta http-equiv="refresh" content="0;url=home.php">';
-        exit();
-      } else {
-        $signUp_error = "Error al registrar el dueño.";
       }
     }
   }
