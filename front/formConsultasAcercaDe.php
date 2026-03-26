caea751cd643979789e914844f12e172a1ec3397
franlovatti <franciscolovatti08@gmail.com>
2026-03-26 19:41:56 -0300

    fix: form contacto con brevo y resend

--- front/formConsultasAcercaDe.php
+++ front/formConsultasAcercaDe.php
@@ -1,12 +1,12 @@
 <?php
-require("scriptFormConsultasAcercaDe.php");
+require __DIR__ . '/scriptFormConsultasAcercaDe.php';
 ?>
 <?php 
   if(isset($_POST['submit'])){
     if(empty($_POST['email']) || empty($_POST['nombre']) || empty($_POST['mensaje'])){
         $response = "All fields are required";
     }else{
-        $response = sendMail($_POST['email'], $_POST['nombre'], $_POST['mensaje']);
+        $response = sendContactMail($_POST['email'], $_POST['nombre'], $_POST['mensaje']);
     }
   }
 ?>
